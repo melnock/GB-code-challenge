a802a2ef57030c177a79c3a02611c82712dcf4ff
github username <github email address>
2022-06-13 13:05:43 -0600

    implement bug fixes and feature additions

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 Our goal is to fix and enhance a Pokedex application. If you are unfamiliar with the world of Pokemon, here is a brief explanation:
 
 > The Pokedex is an electronic device created and designed to catalog and provide information regarding the various species of Pokemon featured in the Pokemon video game, anime and manga series.
- 
+
 [Source](https://pokemon.fandom.com/wiki/Pokedex)
- 
+
 Our version of the Pokedex is able to list and search through Pokemon. However, our search is a bit buggy. Additionally, we want to add a feature that shows a selected Pokemon's details like its **type**, **moves**, and **evolution chain**.
 
 Your time is valuable, and we are extremely appreciative of you participating in this assessment. We're looking to gauge your ability to read and edit code, understand instructions, and deliver features, just as you would during your typical day-to-day work. We expect this test to take no more than one to two hours and ask to complete this work within the next two days. Upon submit, we will review and provide feedback to you regardless of our decision to continue the process.
@@ -35,7 +35,7 @@ When you are finished, please upload your completed work to your Github and invi
 ![](mockup0.png)
 
 ### Details Card
-     
+
 - Clicking "Get Details" for any given Pokemon should render a card that has the Pokemon's `name`, `types`, `moves`, and `evolution chain`
 - Use the api functions defined in `api.js` to retrieve this data. Adding new endpoints or editing existing ones are out of scope
 - The details card should match the below mockup
@@ -48,6 +48,15 @@ Please take some time to answer the following questions. Your answers should go 
 
 - Given more time, what would you suggest for improving the performance of this app?
 
+  - Since we limit the view of the number of pokemon, we could paginate the page to load as the user scrolls
+  - If a user has seen the details for a pokemon, we could cache/store the pokemon details to avoid additional api calls
+
 - Is there anything you would consider doing if we were to go live with this app?
 
+  - I would suggest modifying the `fetchEvolutionChainById` method to just accept the endpoint url that is provided to us in the api response from `fetchPokemonSpeciesByName`
+  - I might also increase the functionality to enable a user more options of getting details (i.e. making the evolutions clickable to see more details or allowing a user to see more than the first few moves available to any pokemon)
+  - I would also probably suggest adding a page title and possibly instructions.
+
 - What was the most challenging aspect of this work for you (if at all)?
+
+  - The most difficult aspect of this project was learning the nuances of the particular api and what source the correct data came from
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchAllPokemon } from "./api";
+import { fetchAllPokemon, fetchPokemonDetailsByName, fetchEvolutionChainById, fetchPokemonSpeciesByName } from "./api";
 
 function App() {
     const [pokemonIndex, setPokemonIndex] = useState([])
@@ -18,52 +18,119 @@ function App() {
         fetchPokemon().then(() => {
             /** noop **/
         })
-    }, [searchValue])
+    }, [])
 
     const onSearchValueChange = (event) => {
         const value = event.target.value
         setSearchValue(value)
-
         setPokemon(
-            pokemonIndex.filter(monster => !monster.name.includes(value))
+            pokemonIndex.filter(monster => monster.name.includes(value))
         )
     }
 
-    const onGetDetails = (name) => async () => {
-        /** code here **/
+    const onGetDetails = async (name) => {
+      const pokemonSpecies = await fetchPokemonSpeciesByName(name)
+      const pokemonDetails = await fetchPokemonDetailsByName(name)
+      // to distill down to the chain id for the specific pokemon selected
+      // (and not modify the api request),
+      // must extract the id from the evolution chain url
+      const chainId = pokemonSpecies.evolution_chain.url
+        .split('/evolution-chain/')[1].split('/')[0]
+      const pokemonEvolutions = await fetchEvolutionChainById(chainId)
+      // store only the information needed
+      setPokemonDetails({
+        moves: pokemonDetails.moves,
+        types: pokemonDetails.types,
+        chain: pokemonEvolutions.chain
+      })
+    }
+
+    const buildPokemonEvolutionChain = () => {
+      if (pokemonDetails?.chain) {
+        const {chain} = pokemonDetails
+        const evolutionArray = [<span>{chain.species.name}</span>]
+        // this method will recursively add the evolutions to the evolutionArray
+        retrieveEvolutionInfo(chain, evolutionArray)
+        return evolutionArray
+      }
+      return []
+    }
+
+    const retrieveEvolutionInfo = (chain, evolutionArray) => {
+      if (chain.evolves_to.length) {
+        evolutionArray.push(<span>{chain.evolves_to[0].species.name}</span>)
+        retrieveEvolutionInfo(chain.evolves_to[0], evolutionArray)
+      }
     }
 
     return (
-        <div className={'pokedex__container'}>
-            <div className={'pokedex__search-input'}>
-                <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
-            </div>
-            <div className={'pokedex__content'}>
-                {pokemon.length > 0 && (
-                    <div className={'pokedex__search-results'}>
-                        {
-                            pokemon.map(monster => {
-                                return (
-                                    <div className={'pokedex__list-item'} key={monster.name}>
-                                        <div>
-                                            {monster.name}
-                                        </div>
-                                        <button onClick={onGetDetails(monster.name)}>Get Details</button>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                )}
-                {
-                    pokemonDetails && (
-                        <div className={'pokedex__details'}>
-                            {/*  code here  */}
-                        </div>
-                    )
-                }
-            </div>
+      <div className={'pokedex__container'}>
+        <div className={'pokedex__search-input'}>
+          <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
         </div>
+        <div className={'pokedex__content'}>
+          {pokemon.length > 0 ? (
+            <div className={'pokedex__search-results'}>
+              {
+                pokemon.map(monster => {
+                    return (
+                      <div className={'pokedex__list-item'} key={monster.name}>
+                        <div>
+                          {monster.name}
+                        </div>
+                        <button
+                         type='button'
+                         aria-label={`get-details-${monster.name}`}
+                         onClick={() => onGetDetails(monster.name)}
+                         >
+                          Get Details
+                        </button>
+                      </div>
+                    )
+                })
+              }
+            </div>
+          ) :
+          (
+            <h3 className='no-results'>
+              No Results Found
+            </h3>
+          )
+        }
+          {
+            pokemonDetails && (
+              <div className={'pokedex__details'}>
+                  <h2>{pokemonDetails.name}</h2>
+                  <div className='pokemon-stats'>
+                    <div className='types'>
+                      <h2>Types</h2>
+                      <ul>
+                        {pokemonDetails.types
+                          .map(type => <li key={type.type.name}>{type.type.name}</li>)
+                        }
+                      </ul>
+                    </div>
+                    <div className='moves'>
+                      <h2>Moves</h2>
+                      <ul>
+                        {pokemonDetails.moves
+                          .slice(0,4)
+                          .map(move => <li key={move.move.name}>{move.move.name}</li>)
+                        }
+                      </ul>
+                    </div>
+                  </div>
+                  <div className='pokemon-evolution'>
+                    <h2>Evolutions</h2>
+                    <div>
+                      {buildPokemonEvolutionChain()}
+                    </div>
+                  </div>
+              </div>
+            )
+          }
+        </div>
+      </div>
     );
 }
 
